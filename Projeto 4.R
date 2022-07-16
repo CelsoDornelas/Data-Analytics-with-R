eadf6851ec34216c03320577461c86b59cc8fdad
Celso Dornelas <88460602+CelsoDornelas@users.noreply.github.com>
2022-07-16 05:37:26 -0300

    Update Projeto 4.R
    Translating

--- Projeto 4.R
+++ Projeto 4.R
@@ -9,15 +9,7 @@ robust
 mvcna(df,con=0.9,cov=0.9,ordering=list("c","PB"))
 mvcna(df,con=0.9,cov=0.9,ordering=list("T","PB"))
 
-#Fazendo a análise de coincidência do pban não foi encontrado nenhum resultado
-#Como não havia resultado com con e cov igual a 1, fiz a análise de robustez
-#Com a análise, percebeu-se que a configuração com maior score era C=1=T=2<->PB=1
-#que quer dizer que países africanos com colonização francesa e com transição 
-#gerenciada têm proibição de recursos.Assim, foi descobert qual outcome
-#deve-se solicitar a análise de CNA.Para fazer novamente a análise CNA,
-#foi colocado con=0.9 e cov =0.9, e ordering ("C","PB") e ("T","PB"), foi
-#alcançado alguns resultados com o outcome PB, inclusive o resultado com 
-#maior robustez de todos os outcomes, mencionado nesse texto
+
 
 #Doing the pban coincidence analysis did not find any results
 #As there was no result with con and cov equal to 1, I did the robustness analysis
